ad45f4e79146131f5501913825747cff1a85dfeb
xizmark <rahulrumalla@gmail.com>
2015-08-02 19:19:01 -0500

    react View daisy chaining

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,15 +1,11 @@
-/**
- * This file provided by Facebook is for non-commercial testing and evaluation purposes only.
- * Facebook reserves all rights not expressly granted.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
- * FACEBOOK BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
- * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+//require('views/dashboard.jsx');
 
+React.render(
+  <DashBoard />,
+  document.getElementById('application')
+  );
+
+/*
 var Comment = React.createClass({
   render: function() {
     var rawMarkup = marked(this.props.children.toString(), {sanitize: true});
@@ -120,7 +116,9 @@ var CommentForm = React.createClass({
   }
 });
 
+
 React.render(
   <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('content')
 );
+*/
---
+++ src/app/components/welcomeBanner.jsx
@@ -0,0 +1,13 @@
+var WelcomeBanner = React.createClass({
+	render: function(){
+		return (
+		    <div className="jumbotron center-block text-center">
+		        <h3 className="">Welcome to REDDO</h3>
+		        <p>
+		            Ask anything.
+		            <br/> This is a Q and A site using ReactJS and NodeJS
+		        </p>
+		    </div>
+		);
+	}
+});
